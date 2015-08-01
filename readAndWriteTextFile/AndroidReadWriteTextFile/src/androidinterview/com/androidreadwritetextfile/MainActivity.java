ef59ffe31bc6f8bc4b3e85e93745534928aeb978
tamzi <tamrefrank@gmail.com>
2015-08-01 03:09:05 +0300

    Android read and write to file sample

---
+++ readAndWriteTextFile/AndroidReadWriteTextFile/src/androidinterview/com/androidreadwritetextfile/MainActivity.java
@@ -0,0 +1,67 @@
+package androidinterview.com.androidreadwritetextfile;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	EditText textmsg;
+	static final int READ_BLOCK_SIZE = 100;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		textmsg=(EditText)findViewById(R.id.editText1);
+	}
+	
+	// write text to file
+	public void WriteBtn(View v) {
+		// add-write text into file
+		try {
+			FileOutputStream fileout=openFileOutput("mytextfile.txt", MODE_PRIVATE);
+			OutputStreamWriter outputWriter=new OutputStreamWriter(fileout);
+			outputWriter.write(textmsg.getText().toString());
+			outputWriter.close();
+			
+			//display file saved message
+			Toast.makeText(getBaseContext(), "File saved successfully!",
+			Toast.LENGTH_SHORT).show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	// Read text from file
+	public void ReadBtn(View v) {
+		//reading text from file
+		try {
+			FileInputStream fileIn=openFileInput("mytextfile.txt");
+			InputStreamReader InputRead= new InputStreamReader(fileIn);
+			
+			char[] inputBuffer= new char[READ_BLOCK_SIZE];
+			String s="";
+			int charRead;
+			
+			while ((charRead=InputRead.read(inputBuffer))>0) {
+				// char to string conversion
+				String readstring=String.copyValueOf(inputBuffer,0,charRead);
+				s +=readstring;					
+			}
+			InputRead.close();
+			Toast.makeText(getBaseContext(), s,Toast.LENGTH_SHORT).show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
