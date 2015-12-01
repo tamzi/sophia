9300b1e65cddbe7e4ae1a50cd4c04f246093ef8f
tamzi <tamrefrank@gmail.com>
2015-12-01 09:20:57 +0300

    Added Android Chat Demo

---
+++ AndroidChatDemo/ChattDemo Main/gen/com/chatt/demo/BuildConfig.java
@@ -0,0 +1,13 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chatt.demo;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class BuildConfig.
+ */
+public final class BuildConfig
+{
+
+	/** The Constant DEBUG. */
+	public final static boolean DEBUG = true;
+}
