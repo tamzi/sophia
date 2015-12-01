b8a9e0dcc44c9e60b6ad1e5a8dd24b831a63a2f0
tamzi <tamrefrank@gmail.com>
2015-12-01 09:23:02 +0300

    Added Screen2ScreenActivity sample

---
+++ ScreenToScreenActivity/MyAndroidApp/src/com/mkyong/android/App2Activity.java
@@ -0,0 +1,17 @@
+package com.mkyong.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class App2Activity extends Activity {
+
+	Button button;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main2);
+	}
+
+}
