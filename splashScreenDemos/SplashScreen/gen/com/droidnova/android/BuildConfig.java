c2ff6ca9590761360458d49cc7435f1a8c8c84e0
tamzi <tamrefrank@gmail.com>
2015-12-10 09:32:49 +0300

    Added Splash Screen Demos

---
+++ splashScreenDemos/SplashScreen/gen/com/droidnova/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.droidnova.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ splashScreenDemos/SplashScreen/src/com/droidnova/android/MyApp.java
@@ -0,0 +1,14 @@
+package com.droidnova.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyApp extends Activity {
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
